685b23f46eaaf836422abf8d94c51f7aa049d6c4
volkanaktas34 <volkan.aktas@outlook.com.tr>
2026-01-31 12:24:01 +0300

    libft

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: vaktas<vaktas@student.42istanbul.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 18:03:45 by vaktas            #+#    #+#             */
-/*   Updated: 2026/01/30 14:25:13 by vaktas           ###   ########.tr       */
+/*   Updated: 2026/01/31 12:21:00 by vaktas           ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstnew(void *content)
 	node = (t_list *)malloc(sizeof(t_list));
 	if (!node)
 		return (NULL);
-	node->content = ft_strdup((char *)content);
+	node->content = content;
 	node->next = NULL;
 	return (node);
 }
